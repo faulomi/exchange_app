a095222315e618b9f3bc454d39285f8e346533f6
abhisheksoni <abhisheksoni500@gmail.com>
2017-12-22 18:35:42 +0530

    changes

--- api/therocktrading.py
+++ api/therocktrading.py
@@ -2,14 +2,15 @@ import json
 import time
 import requests
 
-headers ={'Content-Type': 'application/json',
-          'User-Agent': 'Mozilla/5.0'}
+headers = {
+    'Content-Type': 'application/json',
+    'User-Agent': 'Mozilla/5.0'
+}
 
 
 def api_query():
     ret = requests.get('https://api.therocktrading.com/v1/funds/tickers', headers=headers)
     data = json.loads(ret.text)['tickers']
-    print(data)
     d = {}
     for item in data:
         d[item['fund_id']] = {
@@ -20,4 +21,4 @@ def api_query():
         }
     return d
 
-api_query()
+# api_query()
