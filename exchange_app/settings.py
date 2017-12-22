1718801903e2411d6d8924991f1d1e99005bf7ce
abhisheksoni <abhisheksoni500@gmail.com>
2017-12-22 14:39:06 +0530

    settings added for humalize

--- exchange_app/settings.py
+++ exchange_app/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'rest_data',
-    'django_extensions'
+    # 'django_extensions'
 )
 
 MIDDLEWARE_CLASSES = (
